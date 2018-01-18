15f9d0edc58cbc6e3bc788de886bc96c04dfc134
Carl Davidson <diadophus@gmail.com>
2018-01-18 09:59:36 -0600

    building back erosion model since things work now
    Looks like changes to gradient finally got it working.

--- noncompiled/academics/TectonicsModeling.js
+++ noncompiled/academics/TectonicsModeling.js
@@ -15,17 +15,10 @@ TectonicsModeling.get_thickness = function(sima, sial, sediment, age, thickness,
 
 	var sima_density = scratch;
 	TectonicsModeling.get_subductable_density(age, sima_density);
-	var sima_thickness = thickness;
-	ScalarField.div_field(sima, sima_density, sima_thickness);
-	ScalarField.add_field(thickness, sima_thickness, 	 thickness);
-
-	var sial_thickness = scratch;
-	ScalarField.div_scalar(sial, 2700, sial_thickness);
-	ScalarField.add_field(thickness, sial_thickness, 	 thickness);
-
-	var sediment_thickness = scratch;
-	ScalarField.div_scalar(sediment, 2500, sediment_thickness);
-	ScalarField.add_field(thickness, sediment_thickness, thickness);
+	
+	ScalarField.div_field(sima, sima_density, thickness);
+	ScalarField.add_scalar_term(thickness, sial, 1/2700, thickness);
+	ScalarField.add_scalar_term(thickness, sediment, 1/2700, thickness);
 
 	return thickness;
 }
@@ -56,7 +49,7 @@ TectonicsModeling.get_density = function(sima, sial, sediment, age, result, scra
 	ScalarField.div_scalar(sial, 2700, sial_thickness);
 
 	var sediment_thickness = scratch2;
-	ScalarField.div_scalar(sediment, 2500, sediment_thickness);
+	ScalarField.div_scalar(sediment, 2700, sediment_thickness);
 
 	var total_thickness = scratch;
 	ScalarField.add_field(sial_thickness, sima_thickness, total_thickness);
@@ -133,16 +126,15 @@ TectonicsModeling.get_weathering_rate = function(sial, sediment, displacement, s
 	var weathering_factor = 1.8e-7; 
 	// ^^^ the rate of weathering per the rate of rainfall in that place
 	// measured in fraction of height difference per meters of rain per million years
-	var critical_sediment_thickness = 1;
+	var critical_sediment_thickness = 10;
 	// ^^^ the sediment thickness (in meters) at which bedrock weathering no longer occurs
 
 	var sial_density = 2700; // kg/m^3
-	var sediment_density = 2500 // kg/m^2, from Simoes et al. 2010
+	var sediment_density = 2700 // kg/m^2, from Simoes et al. 2010
 	var earth_surface_gravity = 9.8; // m/s^2
 	var surface_gravity = 9.8; // m/s^2
 	
-	var water_height = scratch;
-	ScalarField.max_scalar(displacement, sealevel, water_height);
+	var water_height = ScalarField.max_scalar(displacement, sealevel);
 
 	var height_gradient = ScalarField.gradient(water_height);
 	// NOTE: result array does double duty for performance reasons
@@ -157,19 +149,20 @@ TectonicsModeling.get_weathering_rate = function(sial, sediment, displacement, s
 		precipitation * 				// apply precip to get height change
 		sial_density * 					// apply density to get mass converted to sediment
 		surface_gravity/earth_surface_gravity, //correct for planet's gravity
-		result) 
+		result)
 	
-	var bedrock_coverage = scratch;
-	ScalarField.mult_scalar(sediment, -1/(sediment_density * critical_sediment_thickness), bedrock_coverage);
-	ScalarField.add_scalar(bedrock_coverage, 1, bedrock_coverage);
-	ScalarField.max_scalar(bedrock_coverage, 0, bedrock_coverage);
+	var bedrock_exposure = scratch;
+	ScalarField.div_scalar(sediment, -sediment_density * critical_sediment_thickness, bedrock_exposure);
+	ScalarField.add_scalar(bedrock_exposure, 1, bedrock_exposure);
+	ScalarField.max_scalar(bedrock_exposure, 0, bedrock_exposure);
 
-	ScalarField.mult_field(result, bedrock_coverage, result);
+	ScalarField.mult_field(result, bedrock_exposure, result);
 	
 	ScalarField.min_field(result, sial, result);
 	ScalarField.max_scalar(result, 0, result);
 	return result;
 }
+var water_discharge = 1e6;
 // "erosion" is the process by which sediment is transported
 TectonicsModeling.get_erosion_rate = function(sediment, displacement, sealevel, result, scratch){
 	var grid = displacement.grid;
@@ -183,17 +176,66 @@ TectonicsModeling.get_erosion_rate = function(sediment, displacement, sealevel, 
 	var earth_surface_gravity = 9.8; // m/s^2
 	var surface_gravity = 9.8; // m/s^2
 
-	var sediment_density = 2700 // kg/m^2, from Simoes et al. 2010
-
+	var sediment_density = 2700 // kg/m^2, from Simoes et al. 2010 
+	
 	var water_height = ScalarField.max_scalar(displacement, sealevel);
 	var gradient = ScalarField.gradient(water_height);
-	var sediment_flux_metric = VectorField.mult_scalar(gradient, -1);
-	VectorField.mult_scalar_field(sediment_flux_metric, sediment, sediment_flux_metric);
-	var divergence = result; 
-	VectorField.divergence(sediment_flux_metric, divergence);
-	ScalarField.mult_scalar(divergence, ER, divergence);
-	ScalarField.sub_scalar(divergence, Float32Dataset.average(divergence), divergence);
-	return divergence;
+
+	// find slope in meters per radian
+	var greatest_slope = VectorField.magnitude(gradient); 
+	// find slope in meters per meter (divide by radius in meters)
+	ScalarField.div_scalar(greatest_slope, -world.radius, greatest_slope); 
+
+	var greatest_slope_direction = VectorField.normalize(gradient); 
+
+	//// "water discharge" is the volumetric quantity of water that flows downhill through a point 
+	//// TODO: turn this into a scratch param 
+	//water = Float32Raster(displacement.grid); 
+	//water_flow = VectorRaster(displacement.grid); 
+	//water_flux = Float32Raster(displacement.grid); 
+	//water_discharge = Float32Raster(displacement.grid); 
+	//// start with precip 
+	//Float32Raster.fill(water, precip); 
+	//Float32Raster.fill(water_discharge, precip); 
+	//// repeatedly apply height-governed convection (∇⋅(Qw dx ∇h/|∇h|))  
+	//// where Qw is water discharge per timestep, dx is grid cell length, and h is height 
+	//// TODO: turn this into a scratch param 
+	//for (var i = 0; i < 5; i++) { 
+	//	VectorField.mult_scalar_field(greatest_slope_direction, water, water_flow); 
+	//	VectorField.divergence(water_flow, water_flux); 
+	//	ScalarField.mult_scalar(water_flux, grid.average_distance, water_flux); 
+	//	ScalarField.add_field(water, water_flux, water); 
+	//	ScalarField.max_scalar(water_flux, 0, water_flux); 
+	//	ScalarField.add_field(water_discharge, water_flux, water_discharge); 
+	//}
+
+	// Great Scott! It's the flux capacity, Marty! 
+	// "flux capacity" is the maximum rate at which sediment can be transported 
+	//var sediment_flux_capacity = scratch; 
+	//ScalarField.mult_field( 
+	//greatest_slope, 
+	//water_discharge, 
+	//scratch 
+	//) 
+	var sediment_flux_capacity = ScalarField.mult_scalar( 
+	greatest_slope,
+	// scratch,
+	water_discharge * 
+	erosive_factor *   // apply erosive factor to get volume flux of sediment 
+	sediment_density *  // apply sediment density to get mass flux of sediment 
+	surface_gravity/earth_surface_gravity // correct for planet's gravity 
+	); 
+
+	// "flux magnitude" is the actual quantity at which sediment is transported 
+	// i.e. you can't transport more sediment than what exists in a cell 
+	var sediment_flux_magnitude = ScalarField.min_field(sediment_flux_capacity, sediment); 
+
+	var sediment_flux = VectorField.mult_scalar_field(greatest_slope_direction, sediment_flux_magnitude); 
+
+	var erosion_rate = result;
+	VectorField.divergence(sediment_flux, erosion_rate); // apply divergence to get kg/My 
+	ScalarField.mult_scalar(erosion_rate, grid.average_distance*.5, erosion_rate); // apply average distance between cells to get kg * radians/My 
+	return erosion_rate; 
 }
 // get a map of plates using image segmentation and binary morphology
 TectonicsModeling.get_plate_map = function(vector_field, segment_num, min_segment_size, segments) {
